58d4eabfdc49ca4b77cb7c13455f5623bd003111
Takumi Fujimoto <takumi@takumium.com>
2016-05-24 08:42:34 -0700

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+var typedoc = require("gulp-typedoc");
+
+gulp.task("default", function () {
+    return tsProject.src()
+        .pipe(ts(tsProject))
+        .js.pipe(gulp.dest("dist"));
+});
+
+gulp.task("typedoc", function() {
+    return gulp
+        .src(["src/**/*.ts"])
+        .pipe(typedoc({
+            module: "commonjs",
+            target: "es5",
+            out: "docs/",
+            name: "cedict-lookup",
+            ignoreCompilerErrors: true,
+            version: true
+        }))
+    ;
+});
